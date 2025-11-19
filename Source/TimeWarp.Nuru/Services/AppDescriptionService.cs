0b1b5b0d5a4df36be1c89ced1c218d66ba93be64
Steven T. Cramer <steven.cramer@timewarp.enterprises>
2025-11-19 13:24:08 +0700

    fix: remove obsolete service files after consolidation
    Clean up deleted AppNameService and AppDescriptionService files
    that were replaced by consolidated ApplicationMetadata class.

--- Source/TimeWarp.Nuru/Services/AppDescriptionService.cs
+++
@@ -1,9 +0,0 @@
-namespace TimeWarp.Nuru;
-
-/// <summary>
-/// Service for providing application description.
-/// </summary>
-internal class AppDescriptionService(string description)
-{
-  public string Description { get; } = description;
-}
--- Source/TimeWarp.Nuru/Services/AppNameService.cs
+++
@@ -1,9 +0,0 @@
-namespace TimeWarp.Nuru;
-
-/// <summary>
-/// Service for providing application name.
-/// </summary>
-internal class AppNameService(string name)
-{
-  public string Name { get; } = name;
-}
